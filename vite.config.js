af57e26f6d656329ced25449c324a7d0bf01685b
artemfarniev2013 <artemfarniev2013@gmail.com>
2025-12-03 14:55:37 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [vue()],
+    base: '/VueNew/', // Убедитесь что путь правильный!
     server: {
         port: 5173
     }
